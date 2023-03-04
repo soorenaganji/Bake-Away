8bf254f7ee87e4113883555669764f22c43bb553
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2023-03-04 12:17:17 +0330

    updating the UI

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full h-24 rounded-t-xl flex items-center justify-center  border-t-2 border-green-400 mt-[40vh] ">
+    <div className="w-full h-24 rounded-t-xl flex items-center justify-center  border-t-2 bg-pink-300 mt-[40vh] ">
       <p> Created With ❤️ By Sourena Ganji &copy; </p>
     </div>
   );
